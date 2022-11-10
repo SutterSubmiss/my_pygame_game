3e527da6816c22ff3ad67a339f28c276b58aef10
SutterSubmiss <84139532+SutterSubmiss@users.noreply.github.com>
2022-11-10 15:22:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,215 +9,208 @@ red = (255,0,0)
 blue = (0,0,255)
 gameDisplay = pygame.display.set_mode((500,500))
 screen = pygame.display.set_mode(resolution)
-objects = []
 
-
-def display(self):
-    a = pygame.transform.scale(pygame.image.load(self.image), self.scale)
-    gameDisplay.blit(a, (self.x_beginning, self.y_beginning))
-
-class Cart:
-    image  ="begining.png"
-    scale = (200,200)
-    x_beginning = 500
-    y_beginning = 100
-    def display(self):
-       display(self)
-
-
-class Object:
-    scale = (50,50)
-    image = "altm.png"
+class Block(pygame.sprite.Sprite):
+    img = 'altm'
     x_beginning = 0
-    y_beginning = 375
-    def display(self):
-        display(self)
+    y_beginning = 300
+    scale = (50,50)
+    def create_image(self, number):
+        return self.img + ".png"
+    def disp(self):
 
-class Character:
- x_beginning = 10
+        block_surface = pygame.image.load(self.create_image("1"))
+        self.image = block_surface
+        gameDisplay.blit(pygame.transform.scale(self.image, self.scale), (self.x_beginning, self.y_beginning))
+Blocks = pygame.sprite.Group
+floor = Block()
+block2 = Block()
+block2.y_beginning -= block2.scale[1]
+block2.x_beginning = 200
+floor.scale = (1000,50)
+blocks = []
+blocks.append(floor)
+blocks.append(block2)
 
- scale = (150,150)
- y_beginning = 275
- flipped = False
- type = "audzia"
- hop = False
- clicked = []
- blocked = False
- pyg = []
- me = pygame.Rect(x_beginning-scale[0],y_beginning-scale[1],scale[0]+1,scale[1]+1)
+class Character(pygame.sprite.Sprite):
+ boris_audzia_far = 0
+ old_baf = 0
+ type = 'audzia'
+ flipped= False
+ def create_image(self,number):
+    return self.type + "/" + self.type + number + ".png"
+ images =[]
+ blocked = [False,False,False,False]
+ def same_axis(self,something,axis):
+     if(axis == 'x'):
+         beginning = something.x_beginning
+         if(self.x_beginning+self.scale[0]-15>=beginning and self.x_beginning+15<=beginning +something.scale[0]):
+
+             return True
+         else:
+             return False
+     elif(axis == 'y'):
+         beginning = something.y_beginning
+         if (self.y_beginning + self.scale[1] >beginning  and  self.y_beginning< beginning + something.scale[1]):
+             return True
+         else:
+             return False
  def collision(self):
-  for i in objects:
-     ob = pygame.Rect(i.x_beginning-i.scale[0],i.y_beginning-i.scale[1],i.scale[0]+1,i.scale[1]+1)
+  for block in blocks:
+     if(self.same_axis(block,'x')):
+      if(self.y_beginning+self.scale[1] >= block.y_beginning and block.y_beginning >self.y_beginning):
+         self.blocked[3] = True
+      else:
+         self.blocked[3] = False
+      if (self.y_beginning  <= block.y_beginning+ block.scale[1] and block.y_beginning < self.y_beginning):
+          self.blocked[2] = True
 
-     if(ob.colliderect(self.me)):
-        self.blocked = True
+     if (self.same_axis(block, 'y')):
+         if (self.x_beginning + self.scale[0] >= block.x_beginning and block.x_beginning >self.x_beginning or self.x_beginning >1000-self.scale[0]):
+             self.blocked[0] = True
 
- moves = [1,1,1,1,2,3,4]
- moves2 = [1,2,2,2,2,3,4]
-
- def gravity(self):
-      objects_y_beginnings = []
-
-      for i in objects:
-          if(self.x_beginning >= i.x_beginning-i.scale[0] and self.x_beginning <= i.x_beginning):
-           objects_y_beginnings.append([i.y_beginning,i.scale[1]])
-      if(objects_y_beginnings !=  []):
-       while(min(objects_y_beginnings)[0]-min(objects_y_beginnings)[1] > self.y_beginning+self.scale[1]):
-             self.y_beginning +=10
+         if (self.x_beginning <= block.x_beginning+block.scale[0] and block.x_beginning <self.x_beginning or self.x_beginning <1):
+             self.blocked[1] = True
 
 
-
-
-
- image = type+"/"+type+"1.png"
-
+ scale = (50, 100)
+ x_beginning = 0
+ y_beginning = 300-scale[1]
+ moves = [1, 1, 1, 1, 2, 3, 4]
+ moves2 = [1, 2, 2, 2, 2, 3, 4]
  audzia_side = False
-
-
- def character(self):
-   y_corpse_place = self.y_beginning-75
-   a = pygame.image.load(self.image)
-   if (self.flipped == True and self.clicked.__contains__("r")):
-       self.flipped = False
-   elif (self.flipped == False and self.clicked.__contains__("l")):
-       self.flipped = True
-   a = pygame.transform.flip(a, self.flipped, False)
-   a2 = pygame.transform.scale(a, self.scale)
-   self.pyg.append(gameDisplay.blit(a2,(self.x_beginning-15,self.y_beginning)))
-
+ clicked = []
+ def disp(self,number):
+     character_surface = pygame.transform.flip(pygame.image.load(self.create_image(number)),self.flipped,False)
+     self.image = character_surface
+     gameDisplay.blit(pygame.transform.scale(self.image,self.scale),(self.x_beginning,self.y_beginning))
 
  def go_right(self):
+         self.flipped = False
 
-     if(self.x_beginning <1000):
-      self.image  = self.type+"/"+self.type+"2.png"
-      if(self.blocked == False):
-        self.x_beginning += 25
-      self.clicked.append("r")
+         character_surface = pygame.image.load(self.create_image("2"))
+         self.image = character_surface
+         if (self.blocked[0] == False):
+             self.x_beginning += 25
+             return "2"
+         else:
+             return "1"
+
  def go_left(self):
+         self.flipped = True
 
-     if (self.x_beginning > 10):
-      self.image  = self.type+"/"+self.type+"2.png"
-      if (self.blocked == False):
-          self.x_beginning -= 25
-      self.clicked.append("l")
+         character_surface = pygame.image.load(self.create_image("2"))
+         self.image = character_surface
+         if (self.blocked[1] == False):
+             self.x_beginning -= 25
+             return "2"
+         else:
+            return "1"
  def go_up(self):
-     self.image  = self.type+"/"+self.type+"3.png"
-     self.y_beginning -=25
+     character_surface = pygame.image.load(self.create_image("3"))
+     self.image = character_surface
+     if (self.blocked[2] == False and not pygame.key.get_pressed()[pygame.K_DOWN] ):
+       self.y_beginning -= 100
+       return "3"
+     else:
+         return "1"
 
-     self.clicked.append("u")
  def go_down(self):
-     self.image  = self.type+"/"+self.type+"3.png"
-     self.y_beginning += 25
-     self.clicked.append("d")
+     character_surface = pygame.image.load(self.create_image("3"))
+     self.image = character_surface
+     if (self.blocked[3] == False and not pygame.key.get_pressed()[pygame.K_UP] ):
+        self.y_beginning += 50
+     return "3"
+
+
+ def gravity(self):
+   if(self.blocked[3] == False):
+       self.y_beginning += 50
 
  def walk(self):
-  self.hop = True
+     keys = pygame.key.get_pressed()
+     self.hop = True
+     ret = "1"
+     if (self.type == "audzia"):
 
-  if(self.type == "audzia"):
-    keys = pygame.key.get_pressed()
-    self.hop = True
-    if(keys[pygame.K_RIGHT]):
-          self.go_right()
+         self.hop = True
 
-    if(keys[pygame.K_UP]):
-        self.go_up()
+         if (keys[pygame.K_RIGHT]):
+             ret = self.go_right()
+             self.blocked[0] = True
 
-    if(keys[pygame.K_DOWN]):
-        self.go_down()
 
-    if (keys[pygame.K_LEFT]):
-        self.go_left()
-  elif(self.type == "boris"):
-     if(self.audzia_side == False):
-        rand = self.moves[randint(0,len(self.moves)-1)]
-     else:
-         rand = self.moves2[randint(0, len(self.moves2) - 1)]
-     if(rand == 1):
-         self.go_right()
-     elif(rand== 2):
-         self.go_left()
-     elif(rand == 3):
-         self.go_up()
-     else:
-         self.go_down()
-     return rand
+         else:
+             self.blocked[0] = False
+
+         if (keys[pygame.K_UP]):
+             ret =self.go_up()
+             self.blocked[2] = True
+
+         else:
+             self.blocked[2] = False
+             if (keys[pygame.K_DOWN]):
+               ret =self.go_down()
+
+         if (keys[pygame.K_LEFT]):
+             ret = self.go_left()
+             self.blocked[1] = True
+
+         else:
+             self.blocked[1] = False
+         return ret
+
+     elif (self.type == "boris"):
+         if (self.audzia_side == False):
+             rand = self.moves[randint(0, len(self.moves) - 1)]
+             self.moves.append(rand)
+         else:
+             rand = self.moves2[randint(0, len(self.moves2) - 1)]
+             self.moves2.append(rand)
+         self.old_baf = self.boris_audzia_far
+         if (audzia.x_beginning < self.x_beginning):
+             self.audzia_side = True
+         else:
+             self.audzia_side = False
+         if ((audzia.x_beginning >= 0 and self.x_beginning >= 0) or (
+                 audzia.x_beginning <= 0 and self.x_beginning <= 0)):
+             self.boris_audzia_far = abs(abs(audzia.x_beginning) - abs(self.x_beginning))
+         else:
+             boris.boris_audzia_far = abs(audzia.x_beginning) + abs(self.x_beginning)
+         if (old_baf > self.boris_audzia_far):
+             if (self.audzia_side == False):
+                 self.moves.append(self.walk())
+
+             else:
+                 self.moves2.append(self.walk())
+         if (rand == 1):
+             return self.go_right()
+         elif (rand == 2):
+             return self.go_left()
+         elif (rand == 3):
+             return  self.go_up()
+         elif(self.blocked[3] == True):
+             return  self.go_down()
+         else:
+            return self.walk()
  def whiletrue(self):
-    if(self.hop == True):
-
-      if(self.clicked.__contains__("r")):
-          self.image  = self.type+"/"+self.type+"1.png"
-      if (self.clicked.__contains__("l")):
-          self.image  = self.type+"/"+self.type+"1.png"
-      if (self.clicked.__contains__("u")):
-          self.image  = self.type+"/"+self.type+"1.png"
-          self.gravity()
-      if (self.clicked.__contains__("d")):
-          self.y_beginning -= 25
-          self.image  = self.type+"/"+self.type+"1.png"
-      self.clicked = []
-      self.hop = False
-    self.walk()
-    self.character()
-    self.gravity()
-    self.blocked = False
-
-i = 0
-screen.fill(white)
-floor = Object()
-floor.scale = (1000, 50)
-carts = []
-second_object = Object()
-second_object.x_beginning = 500
-second_object.y_beginning = 325
-objects.append(second_object)
-objects.append(floor)
-floor.display()
-second_object.display()
+        self.collision()
+        self.gravity()
+        self.disp(self.walk())
+        pygame.time.wait(10)
 audzia = Character()
 boris = Character()
 boris.type = "boris"
-audzia.x_beginning =  300
 old_baf = 0
-beginning_cart = Cart()
-carts.append(beginning_cart)
-beginning_cart.display()
 while True:
- #if(carts == []):
-    audzia.collision()
-    boris.collision()
+    floor.disp()
     audzia.whiletrue()
     boris.whiletrue()
-
-    second_object = Object()
-    floor.scale = (1000, 50)
-    second_object.y_beginning = 325
-    second_object.x_beginning = 500
-    floor.display()
-    second_object.display()
-
-
+    block2.disp()
+    #pygame.time.wait(100)
     pygame.display.flip()
-    if (audzia.x_beginning < boris.x_beginning):
-        boris.audzia_side = True
-    else:
-        boris.audzia_side = False
-    if((audzia.x_beginning >=0 and boris.x_beginning >= 0) or (audzia.x_beginning <= 0 and boris.x_beginning <= 0)):
-     boris_audzia_far = abs(abs(audzia.x_beginning) - abs(boris.x_beginning))
-    else:
-        boris_audzia_far = abs(audzia.x_beginning) + abs(boris.x_beginning)
-    if(old_baf > boris_audzia_far):
-        if(boris.audzia_side == False):
-            boris.moves.append(boris.walk())
-
-        else:
-            boris.moves2.append(boris.walk())
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-    i = 1
-
     screen.fill(white)
-    old_baf = boris_audzia_far
- #elif(pygame.mouse.get_pos()[0] > beginning_cart.x_beginning and pygame.mouse.get_pos()[0] < beginning_cart.x_beginning+beginning_cart.scale[0] and pygame.mouse.get_pos()[1] > beginning_cart.y_beginning and pygame.mouse.get_pos()[1] < beginning_cart.y_beginning+beginning_cart.scale[1]):
-     #carts = []
